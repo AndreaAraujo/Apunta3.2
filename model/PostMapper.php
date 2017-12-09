c5a99df38bcb46ac53500fc268ebf08e5835b6e2
AndreaAraujo <andreaaraujocuquejo@gmail.com>
2017-12-09 17:40:31 +0100

    arreglaoo

--- model/PostMapper.php
+++ model/PostMapper.php
@@ -4,7 +4,7 @@ require_once(__DIR__."/../core/PDOConnection.php");
 
 require_once(__DIR__."/../model/User.php");
 require_once(__DIR__."/../model/Post.php");
-require_once(__DIR__."/../model/Comment.php");
+//require_once(__DIR__."/../model/Comment.php");
 
 /**
 * Class PostMapper
